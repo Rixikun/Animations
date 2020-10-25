0c2915f85637776df348b16dc72c0c7694285434
LinXia <linyianxia@hotmail.com>
2020-10-24 22:02:38 -0400

    include timer, spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useRef} from "react";
 import "./scss/main.scss";
 import Cards from "./component/Cards";
+import Spin from './component/Spin'
+import CountDown from './component/Timer'
+
+const scrollToRef = (ref) => {
+  ref.current.scrollIntoView({behavior: 'smooth'})
+}
 
 function App() {
+  const myRef = useRef(null)
+  const executeScroll = () => scrollToRef(myRef)
+
   return (
     <div className="App">
+      <button onClick={executeScroll}>Jump Down</button>
       <Cards />
-      <footer>
+      <CountDown seconds={5}/>
+      <Spin />
+      <footer ref={myRef}>
         <p>Lin 2020</p>
       </footer>
+      
     </div>
   );
 }
